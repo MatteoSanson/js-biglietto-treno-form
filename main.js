6c7bd317b415985dd1990e48e7b04f9720aa4982
MatteoSanson <matteo.sanson@gmail.com>
2023-11-07 16:21:05 +0100

    feat: prova output

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+
+genera.addEventListener('click', function() {
+            const km = Number(document.getElementById("km").value);
+            const eta = Number(document.getElementById("eta").value);
+
+            const risultato = km + " km --- " + eta + " anni";
+            document.getElementById("risultato").innerHTML = risultato;
+  
+  console.log ('tuoi km', km);
+  console.log ('la tua età', eta);
+  console.log ('risultato', risultato);
+        });
+
+
+// function genera() {
+//     const km = Number(document.getElementById("km").value);
+//     const eta = Number(document.getElementById("eta").value);
+
+//     const risultato = km + " km --- " + eta + " anni";
+//     document.getElementById("risultato").innerHTML = risultato;
+
+//     console.log ("km viaggio: ", km);
+//     console.log ("la tua età: ", eta);
+// }
