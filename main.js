eb1cfd8f355f940b01c59b347884988c2ce4be8e
MatteoSanson <matteo.sanson@gmail.com>
2023-11-07 17:24:56 +0100

    feat: milestone1

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 'use strict';
 
 
-genera.addEventListener('click', function() {
-            const km = Number(document.getElementById("km").value);
-            const eta = Number(document.getElementById("eta").value);
 
-            const risultato = km + " km --- " + eta + " anni";
-            document.getElementById("risultato").innerHTML = risultato;
-  
-  console.log ('tuoi km', km);
-  console.log ('la tua età', eta);
-  console.log ('risultato', risultato);
-        });
+genera.addEventListener('click', 
+    function() {
+        const km = Number(document.getElementById("km").value);
+        const eta = Number(document.getElementById("eta").value);
 
+        // variabili sconto
+        const scontoMin = 20;
+        const scontoOver = 40;
 
-// function genera() {
-//     const km = Number(document.getElementById("km").value);
-//     const eta = Number(document.getElementById("eta").value);
+        // variabile prezzokm 
+        const prezzoKm = 0.21;
+       
+        let prezzoBiglietto = km * prezzoKm;
+        
+        
+        if(eta < 18){
+            console.log ("Hai uno sconto under 18 anni");
+            const scontoBiglietto = ((prezzoBiglietto / 100) * scontoMin);
+            prezzoBiglietto -= scontoBiglietto;
+                
+        }else if (eta >= 65){
+            console.log ("Hai uno sconto over 65 anni");
+            const scontoBiglietto = ((prezzoBiglietto / 100) * scontoOver);
+            prezzoBiglietto -= scontoBiglietto;
+        }else {
+            console.log ("Non hai sconti età");
+            }
+            
+        
+        const risultato = "Il prezzo del tuo biglietto è " + prezzoBiglietto.toFixed(2) + " €";
 
-//     const risultato = km + " km --- " + eta + " anni";
-//     document.getElementById("risultato").innerHTML = risultato;
-
-//     console.log ("km viaggio: ", km);
-//     console.log ("la tua età: ", eta);
-// }
+        console.log (risultato);
+        
+        document.getElementById("risultato").innerHTML = risultato;
+        
+});
